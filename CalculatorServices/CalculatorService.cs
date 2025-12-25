605a4aebcd4a03cb608e10accc9c7ca3a201c788
Esc <vasercom7@gmail.com>
2025-12-25 17:38:22 +0300

    Added application layer, removed unnecessary method from domain layer

---
+++ CalculatorServices/CalculatorService.cs
@@ -0,0 +1,36 @@
+﻿using Calculator.Domain;
+namespace Calculator.CalculatorServices;
+
+public class Calculator : ICalculator
+{
+    public double Add(double x, double y) => x + y;
+    public double Subtract(double x, double y) => x - y;
+    public double Multiply(double x, double y) => x * y;
+    public double Divide(double x, double y) => x / y;
+}
+
+public class CalculatorService
+{
+    private readonly ICalculator _calculator;
+    public CalculatorService(ICalculator calculator) => _calculator = calculator;
+    public double PerformAddition(double x, double y)
+    {
+        return _calculator.Add(x, y);
+    }
+    public double PerformSubtraction(double x, double y)
+    {
+        return _calculator.Subtract(x, y);
+    }
+    public double PerformMultiplication(double x, double y)
+    {
+        return _calculator.Multiply(x, y);
+    }
+    public double PerformDivision(double x, double y)
+    {
+        if (y == 0)
+        {
+            throw new DivideByZeroException("Can't divide by zero!");
+        }
+        return _calculator.Divide(x, y);
+    }
+}
--- Domain/CalculatorData.cs
+++ Domain/CalculatorData.cs
@@ -1,10 +1,9 @@
 ﻿namespace Calculator.Domain;
 
-interface ICalculator
+public interface ICalculator
 {
-    (double, double) GetValues();
     double Add(double x, double y);
-    double Substract(double x, double y);
+    double Subtract(double x, double y);
     double Multiply(double x, double y);
     double Divide(double x, double y);
 }
