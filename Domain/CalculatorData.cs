03aeeaf474bc22ad494fa1c5e6a7c2be62fee345
Esc <vasercom7@gmail.com>
2025-12-25 17:12:46 +0300

    Added first version of Domain

---
+++ Domain/CalculatorData.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Domain;
+
+interface ICalculator
+{
+    (double, double) GetValues();
+    double Add(double x, double y);
+    double Substract(double x, double y);
+    double Multiply(double x, double y);
+    double Divide(double x, double y);
+}
