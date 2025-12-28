3fbe6e45c65adbb03b374ef7c1107b9c4633c398
Esc <vasercom7@gmail.com>
2025-12-28 14:12:46 +0300

    Replaced manual DI with container-based DI

--- Program.cs
+++ Program.cs
@@ -1,18 +1,33 @@
 ﻿using Calculator.CalculatorConsoleUI;
 using Calculator.CalculatorServices;
-
+using Calculator.Domain;
+using Microsoft.Extensions.DependencyInjection;
 class Program
 {
     static void Main()
     {
+        // try
+        // {
+        //     var calculator = new CalculatorClass();
+        //     var service = new CalculatorService(calculator);
+        //     var console = new CalculatorConsole(service);
+        //     console.Run();
+        // }
+        // catch(Exception ex)
+        // {
+        //     Console.WriteLine($"Error: {ex.Message}");
+        // }
+        var services = new ServiceCollection();
+        services.AddTransient<ICalculator, CalculatorClass>();
+        services.AddTransient<CalculatorService>();
+        services.AddSingleton<CalculatorConsole>();
+        var provider = services.BuildServiceProvider();
         try
         {
-            var calculator = new CalculatorClass();
-            var service = new CalculatorService(calculator);
-            var console = new CalculatorConsole(service);
+            var console = provider.GetRequiredService<CalculatorConsole>();
             console.Run();
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             Console.WriteLine($"Error: {ex.Message}");
         }
