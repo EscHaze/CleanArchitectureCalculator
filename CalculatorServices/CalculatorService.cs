a22e411453a06066989bfd92590cf8a3c4555bdf
Esc <vasercom7@gmail.com>
2025-12-28 14:19:33 +0300

    Separated CalculatorService and CalculatorClass classes

--- CalculatorServices/CalculatorService.cs
+++ CalculatorServices/CalculatorService.cs
@@ -1,14 +1,5 @@
 ﻿using Calculator.Domain;
 namespace Calculator.CalculatorServices;
-
-public class CalculatorClass : ICalculator
-{
-    public double Add(double x, double y) => x + y;
-    public double Subtract(double x, double y) => x - y;
-    public double Multiply(double x, double y) => x * y;
-    public double Divide(double x, double y) => x / y;
-}
-
 public class CalculatorService
 {
     private readonly ICalculator _calculator;
